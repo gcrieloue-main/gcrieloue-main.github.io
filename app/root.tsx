af857b134216139c51a48625da398e63a040c9d2
Crieloue Gilles <gilles.crieloue@gmail.com>
2026-05-30 19:11:51 +0200

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,10 @@ export const links: Route.LinksFunction = () => [
     crossOrigin: "anonymous",
   },
   {
+    rel: "icon",
+    href: "/favicon.png",
+  },
+  {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
